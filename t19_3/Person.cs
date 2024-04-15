c68720e81550f9bbb332b6dba006b2c77a26e9ca
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-04-15 09:46:46 +0300

    Add files via upload

---
+++ t19_3/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t19_3
+{
+    class Person : IComparable<Person>
+    {
+        public string Name {  get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public int CompareTo(Person obj)
+        {
+           return Name.CompareTo(obj.Name);
+        }
+        public void OutPut()
+        {
+            Console.WriteLine($"{Name};{Age} лет");
+        }
+    }
+}
---
+++ t19_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace t19_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person("Кирилл",17));
+            people.Add(new Person("Андрей", 17));
+            people.Add(new Person("Даша", 18));
+            foreach(var elem in people)
+            {
+                elem.OutPut();
+            }
+            people.Sort();
+            WriteLine("Отсортированный список");
+            foreach (var elem in people)
+            {
+                elem.OutPut();
+            }
+            Read();
+        }
+    }
+}
