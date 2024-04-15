a9e7c17048ad0e9505169c30b9614054f030388c
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-04-15 09:47:14 +0300

    3 задание

---
+++ t19_3(3)/Autoshop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t19_3_3_
+{
+    class Autoshop : IComparable<Autoshop>
+    {
+        public int Id {  get; set; }
+        public string Marka { get; set; }
+        public int MaxSpeed { get; set; }
+        public double Price { get; set; }
+        public int Discount {  get; set; }
+        public Autoshop() { }
+        public Autoshop(int id, string marka, int maxSpeed, double price, int discount)
+        {
+            Id = id;
+            Marka = marka;
+            MaxSpeed = maxSpeed;
+            Price = price;
+            Discount = discount;
+        }
+        public int CompareTo(Autoshop obj)
+        {
+            return Price.CompareTo(obj.Price);
+        }
+        public void OutPut()
+        {
+            Console.WriteLine($"{Id} ;{Marka} ;{MaxSpeed} ;{Price} $;{Discount} %.");
+        }
+
+        
+    }
+}
---
+++ t19_3(3)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t19_3_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Autoshop> autoshops = new List<Autoshop>();
+            autoshops.Add(new Autoshop(45, "Reno", 180, 12000, 5));
+            autoshops.Add(new Autoshop(124, "Toyota", 220, 5500, 10));
+            autoshops.Add(new Autoshop(156, "BMW", 250, 10500, 3));
+            foreach (var elem in  autoshops) 
+            {
+                elem.OutPut();
+            }
+            autoshops.Sort();
+            Console.WriteLine("\nОтсортированный список");
+            foreach (var elem in autoshops)
+            {
+                elem.OutPut();
+            }
+            Console.ReadLine();
+        }
+    }
+}
